8e2ea984aea0b2e09a583dabfcd739967514c1c9
Alessandro Petitti <99899888+Alessandro-Petitti@users.noreply.github.com>
2023-06-26 20:40:04 +0200

    Add files via upload

---
+++ podometro-fitler_based/podometro-fitler_based.ino
@@ -0,0 +1,105 @@
+#include <M5Stack.h>
+#include <Adafruit_NeoPixel.h>
+#include "utility/MPU9250.h"
+
+#define M5STACK_FIRE_NEO_NUM_LEDS 10
+#define M5STACK_FIRE_NEO_DATA_PIN 15
+#define NOTE_DH2 661
+
+
+
+float accX = 0.0F, accY = 0.0,accZ = 0.0F;  // Define variables for storing inertial sensor data
+//float accY = 0.0F;  
+//float accZ = 0.0F;
+bool acqprima,stato=false, completo_calib = false;
+float accelerazione, alpha = 0.69,filtered_acc = 0, total = 0,avg, width, step_length,  accTot=0, mean ;
+int numeroPassi=0, count, giri, alt, largh;
+
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(
+    M5STACK_FIRE_NEO_NUM_LEDS, M5STACK_FIRE_NEO_DATA_PIN, NEO_GRB + NEO_KHZ800);
+
+
+
+void step_counted()
+{
+    pixels.fill(pixels.Color(0, 7, 0));// colora verde i led
+    M5.Lcd.fillScreen(GREEN);// cambia colore display
+    pixels.show();
+    delay(100);
+    pixels.fill(pixels.Color(0, 0, 0));// colora bianco i led
+    pixels.show();
+    M5.Lcd.fillScreen(BLACK);
+}
+
+void calib_completata ()
+{
+        avg = total / count;
+        width = avg / 7;
+        total = avg;
+        M5.Speaker.tone(NOTE_DH2);
+        delay(1000);
+        M5.Speaker.end();
+        completo_calib = true;
+        Serial.println("calib completata");
+}
+
+void setup() {
+    M5.begin();        // Init M5Core.
+    M5.Power.begin(); 
+    pixels.begin(); 
+    M5.IMU.Init();  // Init IMU sensor.  
+    M5.Lcd.fillScreen(BLACK);  // Set the screen background color to black.
+    M5.Lcd.setTextColor(GREEN, BLACK);  // Sets the foreground color and background color of the
+    M5.Lcd.setTextSize(6);  // Set the font size.
+    //M5.Speaker.setVolume(11);
+}
+double low_pass_filter(double prev_value,double new_value) {
+    return alpha * prev_value + (1 - alpha) * new_value;
+}
+
+void loop()
+ {
+    M5.IMU.getAccelData(&accX, &accY,&accZ);  // Stores the triaxial accelerometer. 
+    accelerazione= sqrt((accX)*(accX)+ (accY)*(accY) +(accZ)*(accZ));
+    accTot+=filtered_acc;
+    giri +=1;
+    filtered_acc = low_pass_filter(filtered_acc, accelerazione);
+    Serial.println(filtered_acc);
+    //Serial.println(accelerazione);
+    // Calibration for average acceleration.
+      if (count < 1000) 
+      {
+        total += filtered_acc;
+        count += 1;
+        //Serial.println(count);
+      } 
+      else if(count >= 1000 && completo_calib == false)
+      {
+        mean=accTot/giri;
+        calib_completata();
+      }
+     if(filtered_acc>avg+width && completo_calib)
+     {
+       stato=true;
+       acqprima=stato;
+     }
+     if(filtered_acc<avg+width && completo_calib){
+       stato=false;
+     }
+    
+     if(stato == false && acqprima == true && completo_calib)
+     {
+       numeroPassi +=1;
+       step_counted();
+       acqprima = false;
+     }
+     M5.Lcd.setCursor((int)(M5.Lcd.width()/2)-155, (int)(M5.Lcd.height()/2)-25);
+     M5.Lcd.printf("%5i" ,numeroPassi);
+     // after the calibration, starts to wait 0.1 seconds for each cycle.
+     //this allows 10 checks for each second, 
+     if (completo_calib == true)
+     {
+     delay(100);
+     }
+ }
